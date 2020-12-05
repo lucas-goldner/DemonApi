b0ce5376f2618565a57f6de13229a3d5933669af
Lucas Goldner <lucas.goldner@googlemail.com>
2020-12-05 19:32:34 +0100

    Adding structs to the api

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+//Demon Struct (Model)
+type Demon struct {
+	ID       int64      `json:"id"`
+	Strength string     `json:"strength"`
+	Weakness string     `json:"weakness"`
+	Imun     string     `json:"imun"`
+	Reflect  string     `json:"reflect"`
+	Level    int64      `json:"level"`
+	Attacks  []*Attacks `json:"attacks"`
+}
+
+type Attacks struct {
+	Name        string `json:"name"`
+	Damage      int64  `json:"damage"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
+
+//Get All Demons
+func getDemons(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	fmt.Println("Hello World")
+	//Init Router
+	r := mux.NewRouter()
+
+	//Route Handlers / Endpoints
+
+	log.Fatal(http.ListenAndServe(":4220", r))
 }
